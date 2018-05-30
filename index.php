c5887da6c5a939e1421da26e25839b5252d20950
khanghyxda <mjsakju@gmail.com>
2018-05-30 16:56:52 +0900

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <h1>My first PHP page</h1>
 
 <?php
-echo file_get_contents("https://qhotel.azurewebsites.net");
+echo file_get_contents("https://qhotel.azurewebsites.net/Home/Ping");
 echo file_get_contents("https://qhotelapi.azurewebsites.net");
 ?>
 
